ce8911feff43db748088133cfda017f5bf213700
iamzainzai <43178932+iamzainzai@users.noreply.github.com>
2023-05-27 17:08:21 +0500

    create form page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsComponent } from './forms/forms.component';
+import { DemoMaterialModule } from './material-module';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +16,9 @@ import { FormsComponent } from './forms/forms.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
